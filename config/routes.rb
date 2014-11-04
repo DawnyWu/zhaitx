5f2703899b5b3f3c95f7fd4003ed8cfa97001219
Dawny Wu <superbatironmans5@gmail.com>
2014-11-04 16:32:00 +0800

    add product model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products
+
   devise_for :users, path_names: { sign_in: 'login',
                                    sign_out: 'logout', 
                                    sign_up: 'register' }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104075820) do
+ActiveRecord::Schema.define(version: 20141104082657) do
+
+  create_table "products", force: true do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  limit: 255, default: "", null: false
