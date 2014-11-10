bfd25e30947062d7e7c79d2c1bccf20f835d9f4f
Dawny Wu <superbatironmans5@gmail.com>
2014-11-10 15:36:39 +0800

    upload image using carrierwave

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,6 +40,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description)
+      params.require(:product).permit(:name, :description, :image, :price)
     end
 end
